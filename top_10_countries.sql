dc308208873a2bc9142c8473670f03d14ce91ce6
Matthew Tsubota <67293324+mtsubota851@users.noreply.github.com>
2022-04-27 10:44:19 -0700

    Delete top_10_countries.sql

--- top_10_countries.sql
+++
@@ -1,9 +0,0 @@
-SELECT D.Country,
-COUNT (customer_id) as customer_count
-FROM customer A
-INNER JOIN address B ON A.address_id = B.address_id
-INNER JOIN city C ON B.city_id = C.city_id
-INNER JOIN country D ON C.country_id = D.country_id
-GROUP BY country 
-ORDER BY customer_count DESC
-LIMIT 10;
